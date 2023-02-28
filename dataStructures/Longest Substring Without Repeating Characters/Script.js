3f19f80ba8cd04823fd11b60ad67e7d06019295b
zzhao003 <zzhao003@gmail.com>
2023-02-28 17:50:28 -0500

    updates Longest

--- dataStructures/Longest Substring Without Repeating Characters/Script.js
+++ dataStructures/Longest Substring Without Repeating Characters/Script.js
@@ -32,9 +32,28 @@ var lengthOfLongestSubstring = function (s) {
   return Math.max(...array);
 };
 
-console.log(lengthOfLongestSubstring("dvdf"));
+// console.log(lengthOfLongestSubstring("dvdf"));
+//Test case = 'abcabcbb'
 
 //sliding window method
 // travese the right pointer and store el in a set and update max length, until duplicate is found
 // then increment the left pointer and continue treversing the right pointer and update max length
 //return max value
+
+const slidingWindow = (arr) => {
+  let l = 0;
+  let max = 0;
+  cache = {};
+  for (let r = 0; r < arr.length; r++) {
+    while (arr[r] in cache) {
+      delete cache[arr[l]];
+      l++;
+    }
+    cache[arr[r]] = 1;
+    max = Math.max(max, r - l + 1);
+  }
+  return max;
+};
+
+const arr = "abcabcbb";
+console.log(slidingWindow(arr));
