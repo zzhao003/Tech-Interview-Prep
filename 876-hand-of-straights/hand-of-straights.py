39555df995aff4ba81cf54141dd415994ce7d3b8
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-07-25 17:21:30 -0400

    Time: 201 ms (93.97%) | Memory: 18.3 MB (17.25%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,51 +1,29 @@
-# class Solution:
-#     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-#         if len(hand) % groupSize:
-#             return False
-#         # make a frequency hash
-#         hashM = {}
-#         for n in hand:
-#             hashM[n] = 1 + hashM.get(n, 0)
-#         #make a minHeap with the keys of hash
-#         minH = list(hashM.keys())
-#         heapq.heapify(minH)
-#         #loop over each el in minHeap to see if their following els are consecutive
-#         while minH:
-#             first = minH[0]
-#             #check the following el
-#             for i in range(first, first + groupSize):
-#                 #check if 1, 2, 3 are in hash, if not return False
-#                 if i not in hashM:
-#                     return False
-#                 #decrement frequency of i in hashM
-#                 hashM[i] -= 1
-#                 if hashM[i] == 0:
-#                     if i != minH[0]:
-#                         return False
-#                     heapq.heappop(minH)
-#             return True
-                
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize:
             return False
-
-        count = {}
+        # make a frequency hash
+        hashM = {}
         for n in hand:
-            count[n] = 1 + count.get(n, 0)
-
-        minH = list(count.keys())
+            hashM[n] = 1 + hashM.get(n, 0)
+        #make a minHeap with the keys of hash
+        minH = list(hashM.keys())
         heapq.heapify(minH)
+        #loop over each el in minHeap to see if their following els are consecutive
         while minH:
             first = minH[0]
+            #check the following el
             for i in range(first, first + groupSize):
-                if i not in count:
+                #check if 1, 2, 3 are in hash, if not return False
+                if i not in hashM:
                     return False
-                count[i] -= 1
-                if count[i] == 0:
+                #decrement frequency of i in hashM
+                hashM[i] -= 1
+                if hashM[i] == 0:
                     if i != minH[0]:
                         return False
                     heapq.heappop(minH)
         return True
+                
 
             
