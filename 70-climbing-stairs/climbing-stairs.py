f344c79f0674a2662fb90f0950775966a9764262
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-09-05 17:58:02 -0400

    Time: 38 ms (65.05%) | Memory: 16.3 MB (65.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,6 +7,6 @@ class Solution:
         #     two = temp 
         # return one
         dp = [1]*(n+1)
-        for i in range(2,n+1):
-            dp[i] = dp[i-2] + dp[i-1]
+        for i in range(1,n):
+            dp[i+1] = dp[i-1] + dp[i]
         return dp[-1]
