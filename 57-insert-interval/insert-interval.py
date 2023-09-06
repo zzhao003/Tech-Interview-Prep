97e08b57aa7ea34d49acc39e66c64bdf6f735076
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-09-06 14:53:01 -0400

    Time: 89 ms (45.04%) | Memory: 19.9 MB (42.02%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,18 +1,30 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # res = []
+        # for i in range(len(intervals)):
+        #     # when newInterval is in front of current interval, append newI and add the res of the intervals
+        #     if newInterval[1] < intervals[i][0]:
+        #         res.append(newInterval)
+        #         return res + intervals[i:]
+        #     # when newInterval is behind current interval, append current interval
+        #     elif newInterval[0] > intervals[i][1]:
+        #         res.append(intervals[i])
+        #     # when overlapping, merge and update newInterval 
+        #     else:
+        #         newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                
+        # res.append(newInterval)
+        # return res
+
         res = []
-        for i in range(len(intervals)):
-            # when newInterval is in front of current interval, append newI and add the res of the intervals
-            if newInterval[1] < intervals[i][0]:
+        for i, n in enumerate(intervals):
+            if n[1] < newInterval[0]:
+                res.append(n)
+            elif newInterval[1] < n[0]:
                 res.append(newInterval)
                 return res + intervals[i:]
-            # when newInterval is behind current interval, append current interval
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            # when overlapping, merge and update newInterval 
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                
+                newInterval = [min(newInterval[0],n[0]),max(newInterval[1],n[1])]
         res.append(newInterval)
         return res
 
