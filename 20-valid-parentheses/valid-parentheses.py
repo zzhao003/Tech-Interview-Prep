32ea9522fb4e439d147a942ba08fddccc601417e
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-09-26 12:11:32 -0400

    Time: 48 ms (7.26%) | Memory: 16.2 MB (96.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,24 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # if len(s) <= 1: return False
-        # stack = []
-        # m = {'[': ']','{': '}','(': ')'}
-        # for el in s:
-        #     # if el in m:
-        #     #     stack.append(el)
-        #     if stack and stack[-1] in m and m[stack[-1]] == el:
-        #         stack.pop()
-        #     else:
-        #         stack.append(el)
-        #     # print(stack)
-        # return not bool(stack)
-        hm = {')':'(','}':'{',']':'['}
-        res = []
-        for c in s:
-            # if c in s, its a closing,then check if top of stack is a matching opening
-            if res and c in hm and res[-1] == hm[c]:
-                res.pop()
-                # everything else, add to stack
+        if len(s) <= 1: return False
+        stack = []
+        m = {'[': ']','{': '}','(': ')'}
+        for el in s:
+            # if el in m:
+            #     stack.append(el)
+            if stack and stack[-1] in m and m[stack[-1]] == el:
+                stack.pop()
             else:
-                res.append(c)
-        return True if len(res) == 0 else False
+                stack.append(el)
+            # print(stack)
+        return not bool(stack)
