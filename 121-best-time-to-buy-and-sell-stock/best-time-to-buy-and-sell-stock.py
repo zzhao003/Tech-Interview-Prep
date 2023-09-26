3895b18ffc2659ef50e674f363766a540663a931
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-09-26 11:35:13 -0400

    Time: 890 ms (27.97%) | Memory: 27.3 MB (85.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -10,28 +10,11 @@ class Solution:
         #         l = r
         #     r += 1
         # return res
-        # l = 0
-        # res = 0
-        # for r in range(len(prices)):
-        #     profit = prices[r]- prices[l]
-        #     if profit < 0:
-        #         l = r
-        #     res = max(res, profit)
-        # return res
-      l = 0 
-      r = 1 
-      
-      # compare nums[l] and nums[r]
-      # initialize a currMax variable
-      currMax = 0
-      
-      while r < len(prices):
-        if prices[l] > prices[r]: 
-          l = r
-          r += 1
-        else: 
-          localDifference = prices[r] - prices[l]
-          currMax = max(currMax,localDifference)
-          r += 1
-          
-      return currMax
+        l = 0
+        res = 0
+        for r in range(len(prices)):
+            profit = prices[r]- prices[l]
+            if profit < 0:
+                l = r
+            res = max(res, profit)
+        return res
