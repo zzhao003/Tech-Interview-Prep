7b85271aece8a23ad8634515fcdf9aef3e8c2634
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-03 23:22:09 -0400

    Time: 923 ms (5.01%) | Memory: 16.3 MB (15.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fib(self, n: int, memo={}) -> int:
+    def fib(self, n: int) -> int:
         # if n == 0:
         #     return 0
         # if n == 1:
@@ -7,12 +7,13 @@ class Solution:
         # return self.fib(n-1) + self.fib(n-2)
 
         # memoiozation
+        memo = {}
         if n in memo:
             return memo[n]
         if n == 0:
             return 0
         if n == 1:
             return 1
-        memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+        memo[n] = self.fib(n-1) + self.fib(n-2)
         return memo[n]
         
