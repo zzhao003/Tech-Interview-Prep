c8fa1e268579d58d151cab0d84ee2ca7afef95d1
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-09 11:24:58 -0400

    Time: 35 ms (83.51%) | Memory: 16.1 MB (79.00%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -6,14 +6,26 @@ class Solution:
         #     return 1
         # return self.fib(n-1) + self.fib(n-2)
 
-        # memoiozation
-        memo = {}
-        if n in memo:
-            return memo[n]
+        # MEMOIZATION
+
+        # memo = {}
+        # if n in memo:
+        #     return memo[n]
+        # if n == 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        # memo[n] = self.fib(n-1) + self.fib(n-2)
+        # return memo[n]
+        
+        # TABULATION
         if n == 0:
             return 0
-        if n == 1:
-            return 1
-        memo[n] = self.fib(n-1) + self.fib(n-2)
-        return memo[n]
-        
+        arr = [0] * (n+1)
+        arr[0] = 0
+        arr[1] = 1
+        print(arr)
+        for i in range(n+1):
+            if i+2 < n+1:
+                arr[i+2] = arr[i] + arr[i+1]
+        return arr[n]
