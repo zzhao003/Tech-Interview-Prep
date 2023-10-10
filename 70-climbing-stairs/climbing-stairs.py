7849552948edd66fa1ebf396e7b646a80c4253ed
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-10 19:26:00 -0400

    Time: 40 ms (41.94%) | Memory: 16.3 MB (16.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -6,7 +6,17 @@ class Solution:
         #     one = one + two
         #     two = temp 
         # return one
-        dp = [1]*(n+1)
-        for i in range(1,n):
-            dp[i+1] = dp[i-1] + dp[i]
-        return dp[-1]
+        # dp = [1]*(n+1)
+        # for i in range(1,n):
+        #     dp[i+1] = dp[i-1] + dp[i]
+        # return dp[-1]
+
+
+        # TABULATION
+        table = [0] * (n+1)
+        table[0] = 1
+        table[1] = 1
+        for i in range(2, n+1):
+            table[i] = table[i-1] + table[i-2]
+
+        return table[n]
