d258815c26ef92af60010f98d70c59fce0f13568
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-10 15:39:56 -0400

    Time: 39 ms (55.14%) | Memory: 16.5 MB (5.28%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,24 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0]*(n+1)
-        dp[1] = nums[0]
-        for i in range(1, n):
-            dp[i+1] = max(dp[i], dp[i-1]+nums[i])
-        print(dp)
-        return dp[-1]
-    
+    def rob(self, nums: List[int], memo = {}) -> int:
+        # n = len(nums)
+        # dp = [0]*(n+1)
+        # dp[1] = nums[0]
+        # for i in range(1, n):
+        #     dp[i+1] = max(dp[i], dp[i-1]+nums[i])
+        # print(dp)
+        # return dp[-1]
+        if tuple(nums) in memo: return memo[tuple(nums)]
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums[0], nums[1])
+  
+#   // recursive call
+#   // case 1: take the first house and skip the second and run recursive calls on the remainding
+        remaindingNums1 = nums[2:]
+        takeFirst = nums[0] + self.rob(remaindingNums1)
+#   // case 2: take the second house
+        remaindingNums2 = nums[3:]
+        takeSecond = nums[1] + self.rob(remaindingNums2)
+        
+        memo[tuple(nums)] = max(takeFirst, takeSecond)
+        return memo[tuple(nums)]
