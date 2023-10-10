d4357f96bb9c71bf4aa1308886f20941c8d4f938
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-10 19:36:36 -0400

    Time: 39 ms (47.14%) | Memory: 16.1 MB (79.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,22 +1,18 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        # one, two = 1, 1
-        # for i in range(n-1):
-        #     temp = one
-        #     one = one + two
-        #     two = temp 
-        # return one
-        # dp = [1]*(n+1)
-        # for i in range(1,n):
-        #     dp[i+1] = dp[i-1] + dp[i]
-        # return dp[-1]
-
-
+    def climbStairs(self, n: int, memo={}) -> int:
         # TABULATION
-        table = [0] * (n+1)
-        table[0] = 1
-        table[1] = 1
-        for i in range(2, n+1):
-            table[i] = table[i-1] + table[i-2]
+        # table = [0] * (n+1)
+        # table[0] = 1
+        # table[1] = 1
+        # for i in range(2, n+1):
+        #     table[i] = table[i-1] + table[i-2]
 
-        return table[n]
+        # return table[n]
+        # MEMOIZATION
+        if n in memo: return memo[n]
+        if n == 1: return 1
+        if n == 0: return 1
+        memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2) 
+        return memo[n]
+
+
