2099a6cd43a9d2b4d2f8f64de5359a4e9d7e8626
Zhixuan Zhao <119907991+zzhao003@users.noreply.github.com>
2023-10-22 21:33:43 -0400

    Time: 43 ms (71.96%) | Memory: 17 MB (75.38%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,44 +6,16 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # q = deque([root])
-        # res = []
-        # while q:
-        #     temp = []
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if node:
-        #             q.append(node.left)
-        #             q.append(node.right)
-        #             temp.append(node.val)
-        #     if temp:
-        #         res.append(temp)
-        # return res
-        def dfs(node,level,hashmap):
-            if node == None:
-                return
-            if level not in hashmap:
-                hashmap[level] = []
-            hashmap[level].append(node.val)
+        if root is None: return None
+        queue = deque([root])
+        res = []
+        while queue:
+            temp = []
+            for i in range(len(queue)):
+                cur = queue.popleft()
+                temp.append(cur.val)
 
-            dfs(node.left, level+1, hashmap)
-            dfs(node.right, level+1, hashmap)
-
-        hashmap = {}
-        dfs(root,0, hashmap)
-        return hashmap.values()
-    	# def dfs(node,level,hashmap):
-      	#     if node == None:
-        #         return
-        #     if level not in hashmap:
-        #   	    hashmap[level] = []
-      
-        #     hashmap[level].append(node.val)
-          
-        #     dfs(node.left, level+1,hashmap)
-        #     dfs(node.right, level+1,hashmap)
-     
-        # hashmap = {}
-        # dfs(root,0,hashmap)
-      
-        # return list(hashmap.values())
+                if cur.left: queue.append(cur.left)
+                if cur.right: queue.append(cur.right)
+            res.append(temp)
+        return res
